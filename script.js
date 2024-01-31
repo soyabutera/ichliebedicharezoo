e91cad08c72a19b9f4475480a0ca64e62349b844
soyabutera <86771261+soyabutera@users.noreply.github.com>
2024-01-31 23:48:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function showMessage(response) {
     noButton.style.position = "absolute";
 
     // Change image source to "gun.gif"
-    document.getElementsByClassName("image")[0].src = "images/gun.gif";
+    document.getElementsByClassName("wein")[0].src = "wein/cute";
 
     // Generate random coordinates within the visible container
     const randomX = Math.max(0, Math.floor(Math.random() * maxWidth));
@@ -34,10 +34,10 @@ function showMessage(response) {
 
     // Update text content, show message, and change image source to "dance.gif"
     const yesMessage = document.getElementById("question");
-    yesMessage.textContent = "LESGOOO see you on the 14th babygirl😘😘";
+    yesMessage.textContent = "Ich freue michhhhh!!! See you on the 14th baby 😘😘";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
-    document.getElementsByClassName("image")[0].src = "images/dance.gif";
+    document.getElementsByClassName("kisskiss")[0].src = "kisskiss/hello";
 
     // Remove yes button
     document.getElementById("yesButton").remove();
